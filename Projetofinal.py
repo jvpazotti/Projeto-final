9e6822d6f031f72ff003833daa5780c738d409be
RafaelMadaras <rafaelam12@al.insper.edu.br>
2020-05-29 16:01:30 -0300

    Retirei sprite (codigo certo)

--- Projetofinal.py
+++ Projetofinal.py
@@ -1,103 +1,36 @@
-import os
-import sys
 import pygame
-import random
-
-CINZA = (127, 127, 127)
-
-def main():
-    pygame.init()  # inicia rotinas do pygame
-
-    surf = pygame.display.set_mode((600, 600)) # crio superficie para o jogo
-
-    pygame.display.set_caption('Jogo Snake')
-
-    arquivo = os.path.join('/Users/rafae/Documents/Insper/DesSoft/Projeto final/Projeto-final', 'Snake.png')
-    arquivo2 = os.path.join('/Users/rafae/Documents/Insper/DesSoft/Projeto final/Projeto-final', 'apple.png')
-    arquivo3 = os.path.join('/Users/rafae/Documents/Insper/DesSoft/Projeto final/Projeto-final', 'body.png')
-   
-    try:
-        Snake = pygame.image.load(arquivo).convert_alpha()
-    except pygame.error:
-        print('Erro ao tentar ler Snake: caneca.png')
-        sys.exit()
-    try:
-        apple = pygame.image.load(arquivo2).convert_alpha()
-    except pygame.error:
-        print('Erro ao tentar ler Snake: caneca.png')
-        sys.exit()
-    try:
-        body = pygame.image.load(arquivo3).convert_alpha()
-    except pygame.error:
-        print('Erro ao tentar ler Snake: caneca.png')
-        sys.exit()
-
-    pos_cobra = [0,0]
-    x = random.randint(20,590) 
-    y = random.randint(20,590) 
-    pos_maca = [x,y]
-
-    delta_cobra={"esquerda":0, "direita":0, "acima":0, "abaixo":0}
-    velocidade_cobra = 0.18
-
-    clock = pygame.time.Clock() # objeto para controle da atualizações de imagens
-
-    # Game Loop
-    while True:
-        delta_time = clock.tick(60) # garante um FPS máximo de 60Hz
-        
-        eventos = pygame.event.get()
-        for evento in eventos:
-            if evento.type == pygame.QUIT:
-                pygame.quit() # terminado a aplicação pygame
-                sys.exit()    # sai pela rotina do sistema
-
-            
-            if evento.type == pygame.KEYDOWN:   #funções de cada tecla
-                if evento.key == pygame.K_LEFT:
-                    delta_cobra["esquerda"] = 1
-                elif evento.key == pygame.K_RIGHT:
-                    delta_cobra["direita"] = 1
-                elif evento.key == pygame.K_UP:
-                    delta_cobra["acima"] = 1
-                elif evento.key == pygame.K_DOWN:
-                    delta_cobra["abaixo"] = 1
-
-            if evento.type == pygame.KEYUP:
-                if evento.key == pygame.K_LEFT:
-                    delta_cobra["esquerda"] = 0
-                if evento.key == pygame.K_RIGHT:
-                    delta_cobra["direita"] = 0
-                elif evento.key == pygame.K_UP:
-                    delta_cobra["acima"] = 0
-                elif evento.key == pygame.K_DOWN:
-                    delta_cobra["abaixo"] = 0
+from random import randint
  
-        surf.fill(CINZA) # preenche a tela com a cor preta  
-        
-        pos_cobra[0] += (delta_cobra["direita"] - delta_cobra["esquerda"]) * velocidade_cobra * delta_time
-        pos_cobra[1] += (delta_cobra["abaixo"] - delta_cobra["acima"]) * velocidade_cobra * delta_time
-            
-        if pos_cobra[0]>surf.get_width()-Snake.get_width():
-                pygame.quit()
-                sys.exit()
-        elif pos_cobra[0]<0:
-                pygame.quit()
-                sys.exit()
-        if pos_cobra[1]>surf.get_height()-Snake.get_width():
-                pygame.quit()
-                sys.exit()
-        if pos_cobra[1]<0:
-                pygame.quit()
-                sys.exit()
-        if pos_cobra == pos_maca:
-            x = random.randint()
-            y = random.randint()
-            pos_maca = [x,y]
-
-        surf.blit(Snake, pos_cobra)
-        surf.blit(apple, pos_maca)
-        pygame.display.flip() # faz a atualização da tela
-        print(pos_cobra,pos_maca)
-if __name__ == '__main__':
-    main()
+branco=(255,255,255)
+preto=(0,0,0)
+vermelho=(255,0,0)
+verde=(0,255,0)
+azul=(0,0,255)
+ 
+try:
+    pygame.init()
+except:
+    print("O modulo pygame não foi inicializado com sucesso")
+ 
+largura=640
+altura=520
+placar=40
+tamanho = 10
+pos_x=randint(0,(largura-tamanho)/10)*10
+pos_y=randint(placar,(altura-tamanho)/10)*10 
+maca_x=randint(0,(largura-tamanho)/10)*10
+maca_y=randint(placar,(altura-tamanho)/10)*10
+velocidade_x=0
+velocidade_y=0
+CobraXY=[]
+Cobracomp=1
+pontos=0
+ 
+fps = pygame.time.Clock()
+fundo = pygame.display.set_mode((largura,altura))
+pygame.display.set_caption("Jogo Snake")
+fonte_texto=pygame.font.SysFont(None, 32)
+texto=fonte_texto.render('Game Over',True,vermelho)
+ 
+jogo = True
+fimdejogo= False
