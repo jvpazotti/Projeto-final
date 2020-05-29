4d914aed3fbfb5535b7c51cdb2e055d9ac6c131f
FaiaxR <62964381+FaiaxR@users.noreply.github.com>
2020-05-29 16:41:47 -0300

    Loop adicionado

--- Projetofinal.py
+++ Projetofinal.py
@@ -34,3 +34,28 @@ texto=fonte_texto.render('Game Over',True,vermelho)
  
 jogo = True
 fimdejogo= False
+
+while jogo:
+    fps.tick(15)
+    fundo.fill(azul)
+    CobraInicio=[]
+    CobraInicio.append(pos_x)
+    CobraInicio.append(pos_y)
+    CobraXY.append(CobraInicio)
+    texto2=fonte_texto.render('Pontuação : '+ str(pontos),True,branco)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            jogo = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT and velocidade_x != tamanho:
+                velocidade_y=0
+                velocidade_x=-tamanho
+            if event.key == pygame.K_RIGHT and velocidade_x != -tamanho:
+                velocidade_y=0
+                velocidade_x=tamanho
+            if event.key == pygame.K_UP and velocidade_y != tamanho:
+                velocidade_x=0
+                velocidade_y=-tamanho
+            if event.key == pygame.K_DOWN and velocidade_y != -tamanho:
+                velocidade_x=0
+                velocidade_y=tamanho
